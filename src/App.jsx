79cd77dd64a9fe14670fd5cee37a50acc6225d68
josiamiranda3 <josiamiranda3@outlook.com>
2024-11-27 15:43:56 -0300

    adicionei o footer e uma parte  do quero doar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header/header'
+import Footer from './components/Footer/footer'
 import './global.scss'
 
 export default function App(){
@@ -7,6 +8,11 @@ export default function App(){
   return(
     <>
     <Header/>
+    <Footer/>
     </>
   )
 }
+
+// o header e o footer ficam aqui pois ficaram fixo no site sem alteração o main ficara como um carrosel
+// o pages são paginas que irão doar 
+
